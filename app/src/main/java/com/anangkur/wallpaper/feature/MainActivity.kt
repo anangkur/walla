b313beef3f5c52421b2c6fb95e975d6c30b08b6f
anangkur <anangk97@gmail.com>
2021-07-22 16:30:49 +0700

    feat(home): setup bottom navigation in MainActivity.kt

--- app/src/main/java/com/anangkur/wallpaper/feature/MainActivity.kt
+++ app/src/main/java/com/anangkur/wallpaper/feature/MainActivity.kt
@@ -2,7 +2,12 @@ package com.anangkur.wallpaper.feature
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.anangkur.wallpaper.R
 import com.anangkur.wallpaper.databinding.ActivityMainBinding
+import com.anangkur.wallpaper.utils.getHomeFragment
+import com.anangkur.wallpaper.utils.getSavedFragment
+import com.anangkur.wallpaper.utils.getSearchFragment
 
 class MainActivity: AppCompatActivity(){
 
@@ -14,5 +19,32 @@ class MainActivity: AppCompatActivity(){
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        setupNavigationView()
+        binding.bottomNav.selectedItemId = R.id.home
+    }
+
+    private fun setupNavigationView() {
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    setFragment(getHomeFragment())
+                    true
+                }
+                R.id.search -> {
+                    setFragment(getSearchFragment())
+                    true
+                }
+                R.id.saved -> {
+                    setFragment(getSavedFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container_view, fragment).commit()
     }
 }
