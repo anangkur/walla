5a1b68187f7523c6bd267be90af8c607f8a44d83
anangkur <anangk97@gmail.com>
2021-07-22 09:02:16 +0700

    refactor(project): remove news module

--- features/news/src/main/java/com/anangkur/wallpaper/news/detail/DetailActivityActionListener.kt
+++
@@ -1,5 +0,0 @@
-package com.anangkur.wallpaper.news.detail
-
-interface DetailActivityActionListener {
-    fun onClickSeeOriginal(url: String)
-}
--- features/news/src/main/java/com/anangkur/wallpaper/news/home/HomeActionListener.kt
+++
@@ -1,7 +0,0 @@
-package com.anangkur.wallpaper.news.home
-
-import com.anangkur.wallpaper.model.ArticleIntent
-
-interface HomeActionListener {
-    fun onClickItem(data: ArticleIntent)
-}
