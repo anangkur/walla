5034f6dc0ea2b81c49dbc98417bd0143427e1c7b
anangkur <anangk97@gmail.com>
2021-07-22 15:53:46 +0700

    feat(home): create SearchFragment.kt

---
+++ features/search/src/main/java/com/anangkur/wallpaper/features/search/SearchFragment.kt
@@ -0,0 +1,17 @@
+package com.anangkur.wallpaper.features.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.anangkur.wallpaper.feature.search.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return FragmentSearchBinding.inflate(inflater, container, false).also { binding = it }.root
+    }
+}
