faee6f34612e6bcb71968d8144dfa4070a030cdf
anangkur <anangk97@gmail.com>
2021-07-28 15:37:42 +0700

    feat(preview): implement PreviewViewModel in PreviewActivity

--- features/preview/src/main/java/com/anangkur/wallpaper/features/preview/PreviewActivity.kt
+++ features/preview/src/main/java/com/anangkur/wallpaper/features/preview/PreviewActivity.kt
@@ -3,10 +3,12 @@ package com.anangkur.wallpaper.features.preview
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.anangkur.wallpaper.features.preview.databinding.ActivityPreviewBinding
 import com.anangkur.wallpaper.presentation.ARGS_CREATOR
 import com.anangkur.wallpaper.presentation.ARGS_IMAGE_URL
 import com.anangkur.wallpaper.presentation.ARGS_TITLE
+import com.anangkur.wallpaper.presentation.features.preview.PreviewViewModel
 import com.anangkur.wallpaper.utils.*
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
@@ -18,6 +20,8 @@ class PreviewActivity: AppCompatActivity() {
     private lateinit var creator: String
     private lateinit var imageUrl: String
 
+    private lateinit var previewViewModel: PreviewViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,8 +31,31 @@ class PreviewActivity: AppCompatActivity() {
 
         getArgs()
         setData()
+        setupViewModel()
+        observeViewModel()
         setAction()
         setBottomSheetExpand()
+    }
+
+    private fun setupViewModel() {
+        previewViewModel = obtainViewModel(PreviewViewModel::class.java)
+    }
+
+    private fun observeViewModel() {
+        previewViewModel.apply {
+            loading.observe(this@PreviewActivity, Observer {
+                setLoadingSave(it)
+            })
+            error.observe(this@PreviewActivity, Observer {
+                showSnackbarShort(it)
+            })
+            success.observe(this@PreviewActivity, Observer {
+                when (it) {
+                    PreviewViewModel.Companion.Action.Delete -> showSnackbarShort(getString(R.string.message_wallpaper_deleted))
+                    PreviewViewModel.Companion.Action.Insert -> showSnackbarShort(getString(R.string.message_wallpaper_saved))
+                }
+            })
+        }
     }
 
     private fun getArgs() {
@@ -58,17 +85,26 @@ class PreviewActivity: AppCompatActivity() {
     private fun setWallpaper() {
         downloadBitmap(
             onLoading = {
-                binding.flipperSet.displayedChild = 1
+                setLoadingSet(true)
             },
             onFailed = {
-                binding.flipperSet.displayedChild = 0
+                setLoadingSet(false)
                 showSnackbarShort(getString(R.string.message_failed_set_wallpaper))
             },
             onResourceReady = {
+                setLoadingSet(false)
                 setWallpaperDevice(it)
                 showSnackbarShort(getString(R.string.message_success_set_wallpaper))
             },
             imageUrl = imageUrl
         )
+    }
+
+    private fun setLoadingSet(isLoading: Boolean) {
+        binding.flipperSet.displayedChild = if (isLoading) 1 else 0
+    }
+
+    private fun setLoadingSave(isLoading: Boolean) {
+        binding.flipperSave.displayedChild = if (isLoading) 1 else 0
     }
 }
