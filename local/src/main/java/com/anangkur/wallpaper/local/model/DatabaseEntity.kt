0f4bb3ec27658aad5a78d5328e6490b7f6ba6ff9
anangkur <anangk97@gmail.com>
2021-07-28 11:10:16 +0700

    feat(local, data): create a new data class Wallpaper.kt in data and mapper in local

--- local/src/main/java/com/anangkur/wallpaper/local/model/DatabaseEntity.kt
+++ local/src/main/java/com/anangkur/wallpaper/local/model/DatabaseEntity.kt
@@ -1,12 +1,13 @@
 package com.anangkur.wallpaper.local.model
 
 import androidx.room.ColumnInfo
+import androidx.room.Database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.anangkur.wallpaper.data.model.Wallpaper
 
 @Entity
 data class DatabaseEntity(
-    @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
     val id: String,
 
@@ -19,3 +20,7 @@ data class DatabaseEntity(
     @ColumnInfo(name = "imageUrl")
     val imageUrl: String
 )
+
+fun Wallpaper.toDatabaseEntity() = DatabaseEntity(title = title, creator = creator, imageUrl = imageUrl, id = id)
+
+fun DatabaseEntity.toWallpaper() = Wallpaper(id = id, title = title, imageUrl = imageUrl, creator = creator)
