f8117c34d86f0cb0f5185e924344ae1b98c6968a
anangkur <anangk97@gmail.com>
2021-07-29 16:47:09 +0700

    feat(remote): generate new response data class from unsplash api

---
+++ remote/src/main/java/com/anangkur/wallpaper/remote/model/unsplash/WallpaperResponse.kt
@@ -0,0 +1,35 @@
+package com.anangkur.wallpaper.remote.model.unsplash
+
+
+import com.google.gson.annotations.SerializedName
+
+data class WallpaperResponse(
+    @SerializedName("blur_hash")
+    val blurHash: String?,
+    @SerializedName("color")
+    val color: String?,
+    @SerializedName("created_at")
+    val createdAt: String?,
+    @SerializedName("current_user_collections")
+    val currentUserCollections: List<Any>?,
+    @SerializedName("description")
+    val description: String?,
+    @SerializedName("height")
+    val height: Int?,
+    @SerializedName("id")
+    val id: String?,
+    @SerializedName("liked_by_user")
+    val likedByUser: Boolean?,
+    @SerializedName("likes")
+    val likes: Int?,
+    @SerializedName("links")
+    val links: Links?,
+    @SerializedName("updated_at")
+    val updatedAt: String?,
+    @SerializedName("urls")
+    val urls: Urls?,
+    @SerializedName("user")
+    val user: User?,
+    @SerializedName("width")
+    val width: Int?
+)
