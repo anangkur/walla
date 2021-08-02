7bd9a2641a252ca3b4b61add98640464a5b3aaed
anangkur <anangk97@gmail.com>
2021-08-02 14:35:28 +0700

    feat(remote): add new method getCollectionPhotos in RemoteRepository.kt

--- data/src/main/java/com/anangkur/wallpaper/data/repository/RemoteRepository.kt
+++ data/src/main/java/com/anangkur/wallpaper/data/repository/RemoteRepository.kt
@@ -7,4 +7,5 @@ interface RemoteRepository {
     suspend fun fetchWallpaper(clientId: String): List<Wallpaper>
     suspend fun fetchCollection(clientId: String): List<Collection>
     suspend fun fetchCollections(clientId: String, page: Int, perPage: Int): List<Collection>
+    suspend fun fetchCollectionPhotos(clientId: String): List<Wallpaper>
 }
--- remote/src/main/java/com/anangkur/wallpaper/remote/RemoteRepository.kt
+++ remote/src/main/java/com/anangkur/wallpaper/remote/RemoteRepository.kt
@@ -25,4 +25,8 @@ class RemoteRepository(private val unsplashService: UnsplashService): RemoteRepo
     override suspend fun fetchCollections(clientId: String, page: Int, perPage: Int): List<Collection> {
         return unsplashService.getCollections(clientId, page, perPage).map { it.toCollection() }
     }
+
+    override suspend fun fetchCollectionPhotos(clientId: String): List<Wallpaper> {
+        return unsplashService.getCollectionPhotos(clientId).map { it.toWallpaper() }
+    }
 }
