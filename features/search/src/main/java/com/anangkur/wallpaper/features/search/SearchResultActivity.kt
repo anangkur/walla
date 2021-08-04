3e66e100ef3354dda73af38ee23e1bd53a244ed5
anangkur <anangk97@gmail.com>
2021-08-04 12:28:28 +0700

    feat(search): implement search in SearchResultActivity.kt

--- features/search/src/main/java/com/anangkur/wallpaper/features/search/SearchResultActivity.kt
+++ features/search/src/main/java/com/anangkur/wallpaper/features/search/SearchResultActivity.kt
@@ -5,20 +5,43 @@ import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.anangkur.wallpaper.R
+import com.anangkur.wallpaper.R as APP_R
+import com.anangkur.wallpaper.feature.search.R as SEARCH_R
 import com.anangkur.wallpaper.data.model.Wallpaper
 import com.anangkur.wallpaper.feature.search.databinding.ActivitySearchResultBinding
 import com.anangkur.wallpaper.presentation.ARGS_COLOR
+import com.anangkur.wallpaper.presentation.features.search.SearchResultViewModel
 import com.anangkur.wallpaper.presentation.getPreviewDialog
+import com.anangkur.wallpaper.utils.obtainViewModel
 import com.anangkur.wallpaper.utils.showSnackbarShort
+import com.anangkur.wallpaper.BuildConfig
 
 class SearchResultActivity : AppCompatActivity() {
+
+    companion object {
+        private fun Int.toColorString() = when (this) {
+            SEARCH_R.color.black -> "black"
+            SEARCH_R.color.white -> "white"
+            SEARCH_R.color.yellow -> "yellow"
+            SEARCH_R.color.orange -> "orange"
+            SEARCH_R.color.red -> "red"
+            SEARCH_R.color.purple -> "purple"
+            SEARCH_R.color.magenta -> "magenta"
+            SEARCH_R.color.green -> "green"
+            SEARCH_R.color.teal -> "teal"
+            SEARCH_R.color.blue -> "blue"
+            else -> null
+        }
+    }
 
     private lateinit var binding: ActivitySearchResultBinding
 
     private lateinit var searchResultAdapter: SearchResultAdapter
+
+    private lateinit var searchResultViewModel: SearchResultViewModel
 
     private var color: Int = -1
 
@@ -27,9 +50,14 @@ class SearchResultActivity : AppCompatActivity() {
 
         binding = ActivitySearchResultBinding.inflate(layoutInflater)
 
+        setContentView(binding.root)
+
         getIntentExtra()
         setupSearch()
         setupRecyclerResult()
+        setupViewModel()
+        observeViewModel()
+        if (color != -1) searchResultViewModel.searchWallpaper(BuildConfig.UNSPLASH_ACCESS_KEY, color = color.toColorString())
     }
 
     private fun getIntentExtra() {
@@ -40,7 +68,7 @@ class SearchResultActivity : AppCompatActivity() {
         binding.searchResult.visibility = if (color == -1) View.VISIBLE else View.GONE
         binding.searchResult.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                if (query.orEmpty().isNotEmpty()) {}
+                if (query.orEmpty().isNotEmpty()) searchResultViewModel.searchWallpaper(BuildConfig.UNSPLASH_ACCESS_KEY, query = query)
                 return true
             }
             override fun onQueryTextChange(newText: String?): Boolean {
@@ -86,8 +114,24 @@ class SearchResultActivity : AppCompatActivity() {
 
     private fun setEmptyResult() {
         binding.flipperResult.displayedChild = 2
-        binding.tvErrorResult.text = getString(R.string.error_empty)
-        binding.ivRefresh.setImageResource(R.drawable.ic_problem)
+        binding.tvErrorResult.text = getString(APP_R.string.error_empty)
+        binding.ivRefresh.setImageResource(APP_R.drawable.ic_problem)
         binding.btnRefresh.setOnClickListener {  }
+    }
+
+    private fun setupViewModel() {
+        searchResultViewModel = obtainViewModel(SearchResultViewModel::class.java)
+    }
+
+    private fun observeViewModel() {
+        searchResultViewModel.loadingSearchResult.observe(this, Observer {
+            if (it) setLoadingResult()
+        })
+        searchResultViewModel.errorSearchResult.observe(this, Observer {
+            setErrorResult(it.ifEmpty { getString(APP_R.string.error_default) })
+        })
+        searchResultViewModel.searchResult.observe(this, Observer {
+            if (it.isEmpty()) setEmptyResult() else setSuccessResult(it)
+        })
     }
 }
